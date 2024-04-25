007b985c212ab92a89b57e98b8eefed9ab138c8d
fumikito <guy@hametuha.com>
2024-04-25 22:37:50 +0900

    Fix test script.

--- tests/test-string.php
+++ tests/test-string.php
@@ -9,10 +9,6 @@ class TestStringHelpers extends WP_UnitTestCase {
 	 * Test string replacers.
 	 */
 	public function test_replacer() {
-		$lazy_loader = \Tarosky\RenderFaster\Services\LazyLoader::get_instance();
-		$src  = file_get_contents( __DIR__ . '/data/img-simple.html' );
-		$dest = file_get_contents( __DIR__ . '/data/img-converted.html' );
-		$converted = $lazy_loader->convert_attributes( $src, 'img', ['skip'], ['post-thumbnail'] );
-		$this->assertEquals( $dest, $converted );
+		$this->assertTrue( class_exists( 'Tarosky\TSCF\Bootstrap' ) );
 	}
 }
